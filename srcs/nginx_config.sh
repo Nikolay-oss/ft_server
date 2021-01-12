80487939becc74edb592da11e7b256400b486d27
Dipoo Kenchur <dkenchur@su-d5.msk.21-school.ru>
2021-01-12 21:52:03 +0300

    pain...

--- srcs/nginx_config.sh
+++ srcs/nginx_config.sh
@@ -8,4 +8,4 @@ rm /etc/nginx/sites-available/default
 mv ./index.html /var/www/dkenchur_server/
 # delete this line 
 mv ./ex.html /var/www/dkenchur_server/
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx_server.key -out /etc/ssl/certs/nginx_server.crt -subj "/C=RU/ST=Moscow/L=Moscow/O=21school/CN=dkenchur"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/certs/nginx_server.key -out /etc/ssl/certs/nginx_server.crt -subj "/C=RU/ST=Moscow/L=Moscow/O=21school/CN=dkenchur"
