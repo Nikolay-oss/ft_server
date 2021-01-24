9c543bb8e70631d4f2884f4cb91e8c6c7dbca32c
dkenchur <nikolai2a@yandex.ru>
2021-01-24 23:22:17 +0300

    idx update

--- srcs/autoindex.sh
+++ srcs/autoindex.sh
@@ -7,45 +7,37 @@ print_error()
 	exit 1
 }
 
+change_idx()
+{
+	if grep -q "autoindex $1;" dkenchur_server
+	then
+		sed -i "s/autoindex $1;/autoindex $2;/" dkenchur_server
+		echo "autoindex $2"
+		service nginx restart
+	else
+		echo "autoindex is already $3"
+	fi
+}
+
 cd /etc/nginx/sites-available
 help="--help"
 if [ -n "$1" ]
 then
-	if [ $1 == $help ]
+	if [ $1 == "on" ]
+	then
+		change_idx off on enabled
+	elif [ $1 == "off" ]
+	then
+		change_idx on off disabled
+	elif [ $1 == $help ]
 	then
 		echo "Usage: autoindex.sh [arg]"
 		echo "Example: autoindex.sh on"
 		echo -e "Args:\n\ton\tenable autoindex\n\toff\t disable autoindex"
 		exit 0
-	fi
-fi
-
-if [ -n "$1" ]
-then
-	if [ $1 == "on" ]
-	then
-		if grep -q "autoindex off;" dkenchur_server
-		then
-			sed -i 's/autoindex off;/autoindex on;/' dkenchur_server
-			echo "autoindex on"
-			service nginx restart
-		else
-			echo "autoindex is already enabled"
-		fi
-	elif [ $1 == "off" ]
-	then
-		if grep -q "autoindex on;" dkenchur_server
-		then
-			sed -i 's/autoindex on;/autoindex off;/' dkenchur_server
-			echo "autoindex off"
-			service nginx restart
-		else
-			echo "autoindex is already disabled"
-		fi
 	else
 		print_error
 	fi
 else
 	print_error
 fi
-cd /
